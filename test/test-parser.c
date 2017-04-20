70c9681e6ab0683f7cf46190bd8ade5f972be784
Camille Oudot <camille.oudot@orange.com>
2017-04-20 16:10:11 +0200

    test: add chunked msg parsing test

--- test/test-parser.c
+++ test/test-parser.c
@@ -1,3 +1,4 @@
+#include <string.h>
 #include <webcom-c/webcom.h>
 
 #include "stfu.h"
@@ -9,7 +10,15 @@ int main(void) {
 	char *str2 = "{\"t\":\"c\",\"d\":{\"t\":\"h\",\"d\":{\"ts\":1492191239182,\"h\":\"\\/test\\/foo?bar=baz\",\"v\":\"5\"}}}";
 	char *str3 = "{\"t\":\"c\",\"d\":{\"t\":\"s\",\"d\":\"the truth is out there\"}}";
 	char *str4 = "{\"t\":\"d\",\"d\":{\"r\":3,\"a\":\"p\",\"b\":{\"p\":\"/brick/23-32\",\"d\":{\"color\":\"white\",\"uid\":\"anonymous\",\"x\":23,\"y\":32}}}}";
+	char *chunked_str5[] = {
+			"{\"t\":\"d\",\"d\":{\"r\":2,\"a\":\"p",
+			"\",\"b\":{\"p\":",
+			"\"/brick/13-37\",\"d\":{\"color\":\"whi",
+			"te\",\"uid\":\"anonymous\",\"x\":23,\"y\":32}}}}",
+	};
+
 	wc_parser_t *parser;
+	int i;
 
 	STFU_INIT();
 
@@ -25,6 +34,18 @@ int main(void) {
 			json_object_to_json_string_ext(msg4.u.data.u.action.u.put.data, JSON_C_TO_STRING_PLAIN),
 			"{\"color\":\"white\",\"uid\":\"anonymous\",\"x\":23,\"y\":32}"
 	);
+
+	STFU_TRUE   ("Create Webcom msg parser", (parser = wc_parser_new()) != NULL);
+	for (i = 0 ; i < 4 ; i++) {
+		if (i != 3) {
+			STFU_TRUE ("Parse a 4-chunks Webcom message", wc_parse_msg_ex(parser, chunked_str5[i], strlen(chunked_str5[i]), &msg5) ==  WC_PARSER_CONTINUE);
+		} else {
+			STFU_TRUE ("Parse the last chunk of a 4-chunks Webcom message", wc_parse_msg_ex(parser, chunked_str5[i], strlen(chunked_str5[i]), &msg5) ==  WC_PARSER_OK);
+		}
+	}
+	STFU_STR_EQ	("Put action path", msg5.u.data.u.action.u.put.path, "/brick/13-37");
+
+	wc_parser_free(parser);
 
 	STFU_SUMMARY();
 
