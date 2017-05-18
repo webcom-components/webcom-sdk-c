9b7d8a8d9dd90e5f5cd509698282417ab55eb74a
Camille Oudot <camille.oudot@orange.com>
2017-05-18 10:02:51 +0200

    legorange: add comments

--- examples/legorange.c
+++ examples/legorange.c
@@ -6,24 +6,39 @@
 #include <webcom-c/webcom.h>
 #include <ev.h>
 
-static void webcom_service_cb(wc_event_t event, wc_cnx_t *cnx, void *data, size_t len, void *user) {
+/* This callback is called by the webcom SDK when events such as "connection is
+ * ready", "connection was closed", or "incoming webcom message" occur on the
+ * webcom connection.
+ */
+static void webcom_service_cb(wc_event_t event, wc_cnx_t *cnx, void *data,
+		size_t len, void *user)
+{
 	wc_msg_t *msg = (wc_msg_t*) data;
 	switch (event) {
 		case WC_EVENT_ON_CNX_ESTABLISHED:
+			/* when the connection is established, register to events in the
+			 * "/brick" path
+			 */
 			wc_listen(cnx, "/brick");
-			printf("\"X Y COLOR\" (1:white, 0:black)> ");
-			fflush(stdout);
 			break;
 		case WC_EVENT_ON_MSG_RECEIVED:
-			if (msg->type == WC_MSG_CTRL && msg->u.ctrl.type == WC_CTRL_MSG_HANDSHAKE) {
-				printf("Got server handshake: server [%s], timestamp [%ld], version [%s]\n",
+			if (msg->type == WC_MSG_CTRL
+					&& msg->u.ctrl.type == WC_CTRL_MSG_HANDSHAKE)
+			{
+				printf("Got server handshake: server [%s], timestamp [%ld], "
+						"version [%s]\n",
 						msg->u.ctrl.u.handshake.server,
 						msg->u.ctrl.u.handshake.ts,
 						msg->u.ctrl.u.handshake.version);
-			} else if (msg->type == WC_MSG_DATA && msg->u.data.type == WC_DATA_MSG_PUSH && msg->u.data.u.push.type == WC_PUSH_DATA_UPDATE_PUT) {
+			} else if (msg->type == WC_MSG_DATA
+					&& msg->u.data.type == WC_DATA_MSG_PUSH
+					&& msg->u.data.u.push.type == WC_PUSH_DATA_UPDATE_PUT)
+			{
 				printf("Got server data update push: path [%s], data\n%s\n",
 						msg->u.data.u.push.u.update_put.path,
-						json_object_to_json_string_ext(msg->u.data.u.push.u.update_put.data, JSON_C_TO_STRING_PRETTY));
+						json_object_to_json_string_ext(
+								msg->u.data.u.push.u.update_put.data,
+								JSON_C_TO_STRING_PRETTY));
 			}
 			break;
 		case WC_EVENT_ON_CNX_CLOSED:
@@ -33,6 +48,10 @@ static void webcom_service_cb(wc_event_t event, wc_cnx_t *cnx, void *data, size_
 	}
 }
 
+/*
+ * libev callback that captures user input on stdin and sends put messages to
+ * the webcom server if the input matches "x y color"
+ */
 static void stdin_cb (EV_P_ ev_io *w, int revents) {
 	wc_cnx_t *cnx = (wc_cnx_t *)w->data;
 	static char buf[2048];
@@ -42,21 +61,20 @@ static void stdin_cb (EV_P_ ev_io *w, int revents) {
 		if(sscanf(buf, "%d %d %d%c", &x, &y, &col, &nl) == 4) {
 			col_str = col ? "white" : "black";
 
+			/* format the path and the data */
 			asprintf(&path, "/brick/%d-%d", x, y);
-			asprintf(&data, "{\"color\":\"%s\",\"uid\":\"anonymous\",\"x\":%d,\"y\":%d}", col_str, x, y);
+			asprintf(&data, "{\"color\":\"%s\",\"uid\":\"anonymous\",\"x\":%d,"
+					"\"y\":%d}", col_str, x, y);
 
-
-			if (wc_push_json_data(cnx, path, data) > 0) {
+			/* send the put message to the webcom server */
 			if (wc_put_json_data(cnx, path, data) > 0) {
 				puts("OK");
 			} else {
 				puts("ERROR");
 			}
+
 			free(path);
 			free(data);
-
-			printf("\"X Y COLOR\" (1:white, 0:black)> ");
-			fflush(stdout);
 		}
 	}
 	if (feof(stdin)) {
@@ -66,7 +84,8 @@ static void stdin_cb (EV_P_ ev_io *w, int revents) {
 	}
 }
 
-static void webcom_cb(EV_P_ ev_io *w, int revents) {
+/* call webcom API when the webcom connection becomes readable */
+static void webcom_socket_cb(EV_P_ ev_io *w, int revents) {
 	wc_cnx_t *cnx = (wc_cnx_t *)w->data;
 
 	if(revents&EV_READ) {
@@ -74,6 +93,12 @@ static void webcom_cb(EV_P_ ev_io *w, int revents) {
 	}
 }
 
+/* send keepalives to the webcom server periodically */
+static void ka_cb(EV_P_ ev_timer *w, int revents) {
+	wc_cnx_t *cnx = (wc_cnx_t *)w->data;
+
+	wc_cnx_keepalive(cnx);
+}
 
 int main(void) {
 	wc_cnx_t *cnx;
@@ -81,19 +106,30 @@ int main(void) {
 	struct ev_loop *loop = EV_DEFAULT;
 	ev_io stdin_watcher;
 	ev_io webcom_watcher;
+	ev_timer ka_timer;
 
-	cnx = wc_cnx_new("io.datasync.orange.com", 443, "/_wss/.ws?v=5&ns=legorange", webcom_service_cb, 0);
+	cnx = wc_cnx_new(
+			"io.datasync.orange.com",
+			443,
+			"/_wss/.ws?v=5&ns=legorange",
+			webcom_service_cb,
+			NULL);
 
 	wc_fd = wc_cnx_get_fd(cnx);
 
 	ev_io_init(&stdin_watcher, stdin_cb, STDIN_FILENO, EV_READ);
 	stdin_watcher.data = cnx;
 	ev_io_start (loop, &stdin_watcher);
-	ev_io_init(&webcom_watcher, webcom_cb, wc_fd, EV_READ);
-	webcom_watcher.data = cnx;
-    ev_io_start (loop, &webcom_watcher);
 
-    ev_run (loop, 0);
+	ev_io_init(&webcom_watcher, webcom_socket_cb, wc_fd, EV_READ);
+	webcom_watcher.data = cnx;
+	ev_io_start (loop, &webcom_watcher);
+
+	ev_timer_init(&ka_timer, ka_cb, 45, 45);
+	ka_timer.data = cnx;
+	ev_timer_start(loop, &ka_timer);
+
+    ev_run(loop, 0);
 
     return 0;
 }
