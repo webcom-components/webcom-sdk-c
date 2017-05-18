588383225ab5d33c9a32f25650900fc551ff8cb4
Camille Oudot <camille.oudot@orange.com>
2017-05-18 13:18:42 +0200

    legorange: update demo

--- examples/legorange.c
+++ examples/legorange.c
@@ -6,42 +6,88 @@
 #include <webcom-c/webcom.h>
 #include <ev.h>
 
+static inline void clear_screen() {
+	printf("\033[2J");
+}
+
+void draw_brick(unsigned x, unsigned y, char color) {
+	printf("\033[%u;%uf\033[%dm%s\033[0m", y, 2*x, (int) color, color ? " ●" : "  ");
+	fflush(stdout);
+}
+
+void on_brick_update(char *key, json_object *data) {
+	int x, y;
+	json_object *jcolor;
+	char *scolor;
+	char ccolor = 37;
+
+	if (sscanf(key, "%d-%d", &x, &y) == 2) {
+		if (json_object_is_type(data, json_type_null)) {
+			draw_brick(x, y, 0);
+		} else {
+			if (json_object_object_get_ex(data, "color", &jcolor)) {
+				scolor = json_object_get_string(jcolor);
+				if (strcmp("green", scolor) == 0) {
+					ccolor = 32;
+				} else if (strcmp("red", scolor) == 0) {
+					ccolor = 31;
+				} else if (strcmp("darkgrey", scolor) == 0) {
+
+				} else if (strcmp("blue", scolor) == 0) {
+					ccolor = 34;
+				} else if (strcmp("yellow", scolor) == 0) {
+					ccolor = 33;
+				} else if (strcmp("brown", scolor) == 0) {
+					ccolor = 31;
+				}
+				draw_brick(x, y, ccolor);
+			}
+		}
+	}
+}
+
+void on_data_update_update_put(wc_push_data_update_put_t *event) {
+	char *key;
+	struct json_object *val;
+	if (strcmp("/brick", event->path) == 0 && event->data != NULL) {
+		json_object_object_foreach(event->data, key, val) {
+			on_brick_update(key, val);
+		}
+	} else if (strncmp("/brick/", event->path, 7) == 0){
+		on_brick_update(event->path + 7, event->data);
+	}
+}
+
 /* This callback is called by the webcom SDK when events such as "connection is
  * ready", "connection was closed", or "incoming webcom message" occur on the
  * webcom connection.
  */
-static void webcom_service_cb(wc_event_t event, wc_cnx_t *cnx, void *data,
+void webcom_service_cb(wc_event_t event, wc_cnx_t *cnx, void *data,
 		size_t len, void *user)
 {
 	wc_msg_t *msg = (wc_msg_t*) data;
+	struct ev_loop *loop = (struct ev_loop*) user;
+
 	switch (event) {
 		case WC_EVENT_ON_CNX_ESTABLISHED:
 			/* when the connection is established, register to events in the
 			 * "/brick" path
 			 */
 			wc_listen(cnx, "/brick");
+			clear_screen();
 			break;
 		case WC_EVENT_ON_MSG_RECEIVED:
-			if (msg->type == WC_MSG_CTRL
-					&& msg->u.ctrl.type == WC_CTRL_MSG_HANDSHAKE)
-			{
-				printf("Got server handshake: server [%s], timestamp [%ld], "
-						"version [%s]\n",
-						msg->u.ctrl.u.handshake.server,
-						msg->u.ctrl.u.handshake.ts,
-						msg->u.ctrl.u.handshake.version);
-			} else if (msg->type == WC_MSG_DATA
+			if (msg->type == WC_MSG_DATA
 					&& msg->u.data.type == WC_DATA_MSG_PUSH
 					&& msg->u.data.u.push.type == WC_PUSH_DATA_UPDATE_PUT)
 			{
-				printf("Got server data update push: path [%s], data\n%s\n",
-						msg->u.data.u.push.u.update_put.path,
-						json_object_to_json_string_ext(
-								msg->u.data.u.push.u.update_put.data,
-								JSON_C_TO_STRING_PRETTY));
+				on_data_update_update_put(&msg->u.data.u.push.u.update_put);
 			}
 			break;
 		case WC_EVENT_ON_CNX_CLOSED:
+			clear_screen();
+			puts("Closed by remote, Bye");
+			ev_break(EV_A_ EVBREAK_ALL);
 			break;
 		default:
 			break;
@@ -52,7 +98,7 @@ static void webcom_service_cb(wc_event_t event, wc_cnx_t *cnx, void *data,
  * libev callback that captures user input on stdin and sends put messages to
  * the webcom server if the input matches "x y color"
  */
-static void stdin_cb (EV_P_ ev_io *w, int revents) {
+void stdin_cb (EV_P_ ev_io *w, int revents) {
 	wc_cnx_t *cnx = (wc_cnx_t *)w->data;
 	static char buf[2048];
 	int x, y, col;
@@ -78,6 +124,7 @@ static void stdin_cb (EV_P_ ev_io *w, int revents) {
 		}
 	}
 	if (feof(stdin)) {
+		clear_screen();
 		puts("Bye");
 		ev_io_stop(EV_A_ w);
 		ev_break(EV_A_ EVBREAK_ALL);
@@ -85,7 +132,7 @@ static void stdin_cb (EV_P_ ev_io *w, int revents) {
 }
 
 /* call webcom API when the webcom connection becomes readable */
-static void webcom_socket_cb(EV_P_ ev_io *w, int revents) {
+void webcom_socket_cb(EV_P_ ev_io *w, int revents) {
 	wc_cnx_t *cnx = (wc_cnx_t *)w->data;
 
 	if(revents&EV_READ) {
@@ -94,7 +141,7 @@ static void webcom_socket_cb(EV_P_ ev_io *w, int revents) {
 }
 
 /* send keepalives to the webcom server periodically */
-static void ka_cb(EV_P_ ev_timer *w, int revents) {
+void ka_cb(EV_P_ ev_timer *w, int revents) {
 	wc_cnx_t *cnx = (wc_cnx_t *)w->data;
 
 	wc_cnx_keepalive(cnx);
@@ -113,7 +160,7 @@ int main(void) {
 			443,
 			"/_wss/.ws?v=5&ns=legorange",
 			webcom_service_cb,
-			NULL);
+			loop);
 
 	wc_fd = wc_cnx_get_fd(cnx);
 
