dd193c2e8247ed8b163212f9db43181461a1f6af
Camille Oudot <camille.oudot@orange.com>
2017-05-19 17:38:17 +0200

    connection: add method to connect through proxy

--- src/connection.c
+++ src/connection.c
@@ -1,7 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
 #include <nopoll.h>
 
 #include "webcom-c/webcom-cnx.h"
@@ -35,7 +39,11 @@ int wc_cnx_on_readable(wc_cnx_t *cnx) {
 
 	if (n > 0) {
 		if (cnx->parser == NULL) {
-			cnx->parser = wc_parser_new();
+			if (*cnx->rxbuf == '{') {
+				cnx->parser = wc_parser_new();
+			} else {
+				return 1;
+			}
 		}
 		switch (wc_parse_msg_ex(cnx->parser, cnx->rxbuf, (size_t)n, &msg)) {
 		case WC_PARSER_OK:
@@ -90,7 +98,90 @@ int wc_cnx_get_fd(wc_cnx_t *cnx) {
 	return cnx->fd;
 }
 
-wc_cnx_t *wc_cnx_new(char *endpoint, int port, char *path, wc_on_event_cb_t callback, void *user) {
+wc_cnx_t *wc_cnx_new_with_proxy(char *proxy_host, uint16_t proxy_port, char *endpoint, uint16_t port, char *path, wc_on_event_cb_t callback, void *user) {
+	wc_cnx_t *res;
+	char sport[6];
+	int proxy_socket;
+	struct hostent *proxy_hostent;
+	struct sockaddr_in proxy_serveraddr;
+	int nread;
+	noPollConnOpts *npopts;
+
+	res = calloc(1, sizeof(wc_cnx_t));
+
+	if (res == NULL) {
+		goto error1;
+	}
+
+	res->np_ctx = nopoll_ctx_new();
+
+	snprintf(sport, 6, "%hu", port);
+
+	sport[5] = '\0';
+
+	if((proxy_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		goto error2;
+	}
+
+	proxy_hostent = gethostbyname(proxy_host);
+	if (proxy_hostent == NULL) {
+		goto error2;
+	}
+
+	/* build the server's Internet address */
+	bzero((char *) &proxy_serveraddr, sizeof(proxy_serveraddr));
+	proxy_serveraddr.sin_family = AF_INET;
+	bcopy((char *)proxy_hostent->h_addr,
+			(char *)&proxy_serveraddr.sin_addr.s_addr, proxy_hostent->h_length);
+	proxy_serveraddr.sin_port = htons(proxy_port);
+
+	if (connect(proxy_socket, (struct sockaddr*)&proxy_serveraddr, sizeof(proxy_serveraddr)) < 0) {
+		goto error2;
+	}
+
+	dprintf(proxy_socket,
+			"CONNECT %s:%hu HTTP/1.1\r\n"
+			"Host: %s:%hu\r\n"
+			"Proxy-Connection: keep-alive\r\n"
+			"Connection: keep-alive\r\n\r\n",
+			endpoint, port, endpoint, port);
+
+	nread = read(proxy_socket, res->rxbuf, 13);
+
+	/* expect "HTTP/1.? 200 *" or die */
+	if (nread != 13 || strncmp(res->rxbuf + 8, " 200 ", 5) != 0) {
+		close(proxy_socket);
+		goto error2;
+	}
+
+	/* empty the socket read buffer */
+	while (recv(proxy_socket, res->rxbuf, WC_RX_BUF_LEN, MSG_DONTWAIT) > 0);
+
+	npopts = nopoll_conn_opts_new();
+	res->np_conn = nopoll_conn_tls_new_with_socket(res->np_ctx, npopts, proxy_socket, endpoint, sport, NULL, path, NULL, NULL);
+	nopoll_conn_opts_free(npopts);
+
+	if (nopoll_conn_is_ok(res->np_conn)) {
+		res->state = WC_CNX_STATE_CREATED;
+		res->user = user;
+		res->callback = callback;
+		res->fd = nopoll_conn_socket(res->np_conn);
+		nopoll_conn_wait_until_connection_ready(res->np_conn, 2);
+		res->state = WC_CNX_STATE_READY;
+		callback(WC_EVENT_ON_CNX_ESTABLISHED, res, NULL, 0, user);
+	} else {
+		goto error2;
+	}
+
+	return res;
+
+error2:
+	free(res);
+error1:
+	return NULL;
+}
+
+wc_cnx_t *wc_cnx_new(char *endpoint, uint16_t port, char *path, wc_on_event_cb_t callback, void *user) {
 	wc_cnx_t *res;
 	char sport[6];
 
