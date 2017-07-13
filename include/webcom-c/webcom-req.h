277ef88dc64a5c8800c033ac98fd54af77d68d51
Camille Oudot <camille.oudot@orange.com>
2017-07-13 11:32:33 +0200

    document the transactional functions

--- include/webcom-c/webcom-req.h
+++ include/webcom-c/webcom-req.h
@@ -6,18 +6,110 @@
 
 typedef enum {WC_REQ_OK, WC_REQ_ERROR} wc_req_pending_result_t;
 
-typedef void (*wc_on_req_result_t) (wc_cnx_t *cnx, int64_t id, wc_action_type_t type, wc_req_pending_result_t status, char *reason);
 
 #define DECLARE_REQ_FUNC(__name, ... /* args */)	\
 	int64_t wc_req_ ## __name(wc_cnx_t *cnx, wc_on_req_result_t callback, __VA_ARGS__);
 
+/**
+ * @defgroup requests Webcom client to server request functions
+ * @{
+ */
+
+/**
+ * callback type for request status notification
+ *
+ * @param cnx the webcomm connection
+ * @param id the request id
+ * @param type what kind of action (wc_action_type_t) is this notification
+ * about
+ * @param status did the request succeed (WC_REQ_OK) or failed (WC_REQ_ERROR)
+ * @param reason the success or error reason string
+ */
+typedef void (*wc_on_req_result_t) (wc_cnx_t *cnx, int64_t id, wc_action_type_t type, wc_req_pending_result_t status, char *reason);
 
 DECLARE_REQ_FUNC(auth, char *cred)
 DECLARE_REQ_FUNC(unauth, ...)
-DECLARE_REQ_FUNC(put, char *path, char *json)
-DECLARE_REQ_FUNC(merge, char *path, char *json)
-DECLARE_REQ_FUNC(push, char *path, char *json)
-DECLARE_REQ_FUNC(listen, char *path)
-DECLARE_REQ_FUNC(unlisten, char *path)
+
+/**
+ * sends a data put request to the webcom server and get notified of the status
+ *
+ * This function builds and sends a data put request to the webcom server.
+ *
+ * @param cnx the webcom connection
+ * @param callback callback that will be called when the status of the request
+ * is sent back from the server
+ * @param path a string representing the path of the data
+ * @param json a string containing the JSON-encoded data to store on the server
+ * 	             at the given path
+ * @return the put request id (>0) if it was sent successfully, -1 otherwise
+ */
+int64_t wc_req_put(wc_cnx_t *cnx, wc_on_req_result_t callback, char *path, char *json);
+
+/**
+ * sends a data merge request to the webcom server and get notified of the
+ * status
+ *
+ * This function builds and sends a data merge request to the webcom server.
+ *
+ * @param cnx the webcom connection
+ * @param callback callback that will be called when the status of the request
+ * is sent back from the server
+ * @param path a string representing the path of the data
+ * @param json a string containing the JSON-encoded data to merge on the server
+ * 	             at the given path
+ * @return the put request id (>0) if it was sent successfully, -1 otherwise
+ */
+int64_t wc_req_merge(wc_cnx_t *cnx, wc_on_req_result_t callback, char *path, char *json);
+
+/**
+ * sends a data push request to the webcom server and get notified of the status
+ *
+ * This function builds and sends a data push request to the webcom server. It
+ * slightly differs from the put request: in the push case, the SDK adds a
+ * unique, time-ordered, partly random key to the path and sends a put request
+ * to this new path.
+ *
+ * @param cnx the webcom connection
+ * @param callback callback that will be called when the status of the request
+ * is sent back from the server
+ * @param path a string representing the path of the data
+ * @param json a string containing the JSON-encoded data to push
+ *
+ * @return the put request id (>0) if it was sent successfully, -1 otherwise
+ */
+int64_t wc_req_push(wc_cnx_t *cnx, wc_on_req_result_t callback, char *path, char *json);
+
+/**
+ * sends a listen request to the webcom server and get notified of the status
+ *
+ * This function builds and sends a listen request to the webcom server.
+ *
+ * @param cnx the webcom connection
+ * @param callback callback that will be called when the status of the request
+ * is sent back from the server
+ * @param path a string representing the path to listen to
+ *
+ * @return the put request id (>0) if it was sent successfully, -1 otherwise
+ */
+int64_t wc_req_listen(wc_cnx_t *cnx, wc_on_req_result_t callback, char *path);
+
+/**
+ * sends a un-listen request to the webcom server and get notified of the status
+ *
+ * This function builds and sends a un-listen request to the webcom server, to
+ * stop listening a given path.
+ *
+ * @param cnx the webcom connection
+ * @param callback callback that will be called when the status of the request
+ * is sent back from the server
+ * @param path a string representing the path
+ *
+ * @return the put request id (>0) if it was sent successfully, -1 otherwise
+ */
+int64_t wc_req_unlisten(wc_cnx_t *cnx, wc_on_req_result_t callback, char *path);
+
+/**
+ * @}
+ */
 
 #endif /* INCLUDE_WEBCOM_C_WEBCOM_REQ_H_ */
