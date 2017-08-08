7a850080ecad0d05d4e15fcbca763f20566dcaa8
Camille Oudot <camille.oudot@orange.com>
2017-08-08 11:26:38 +0200

    Dockerfiles for building in several environments
    currently supported:
    - debian 9
    - ubuntu 16.04, 16.10, 17.04
    - fedora 24, 25, 26
    - centos 7

---
+++ build-images/build.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -e
+set -x
+
+BUILD_ROOT='/var/build'
+
+git_url=${git_url:-https://www.forge.orange-labs.fr/plugins/git/webcom/webcom-sdk-c.git}
+git_ref=${git_ref:-master}
+package_flavour="${package_flavour:-0}"
+
+if cmake --version ; then
+	CMAKE=cmake
+else
+	if cmake3 --version ; then
+		CMAKE=cmake3
+	else
+		exit 1
+	fi
+fi
+
+mkdir -p "$BUILD_ROOT"
+
+cd "$BUILD_ROOT"
+
+repo=$(basename "$git_url" .git)
+
+if [ -d "$repo" ] ; then
+	cd "$repo"
+	git fetch
+else
+	git clone "$git_url"
+	cd "$repo"
+fi
+
+git checkout "$git_ref"
+git merge "origin/$git_ref"
+
+cd build
+
+"$CMAKE" .. "-DTARGET_PACKAGE_FLAVOUR=${package_flavour}"
+make clean all
+make package
---
+++ pkg/pkg.cmake
@@ -0,0 +1,74 @@
+#### webcom-c-sdk packaging
+
+set(CPACK_PACKAGE_NAME "webcom-sdk-c")
+set(CPACK_PACKAGE_VERSION ${PROJECT_VERSION})
+set(CPACK_PACKAGE_CONTACT "Camille Oudot <camille.oudot@orange.com>")
+set(CPACK_PACKAGE_VENDOR "Orange S.A.")
+set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "Webcom C SDK - dynamic library and development headers")
+set(CPACK_PACKAGE_DESCRIPTION_FILE "${CMAKE_SOURCE_DIR}/pkg/description.txt")
+
+if(TARGET_PACKAGE_FLAVOUR)
+	if(TARGET_PACKAGE_FLAVOUR STREQUAL "debian9")
+		set(CPACK_GENERATOR "DEB")
+		set(CPACK_DEBIAN_PACKAGE_DEPENDS  "libwebsockets8, libjson-c3, libssl1.1, libev4, libncurses5, libc6, ca-certificates")
+		set(CPACK_SYSTEM_NAME "deb9")
+
+	elseif(TARGET_PACKAGE_FLAVOUR STREQUAL "ubuntu16.04")
+		set(CPACK_GENERATOR "DEB")
+		set(CPACK_DEBIAN_PACKAGE_DEPENDS  "libwebsockets7, libjson-c2, libssl1.0.0, libev4, libncurses5, libc6, ca-certificates")
+		set(CPACK_SYSTEM_NAME "xenial")
+
+	elseif(TARGET_PACKAGE_FLAVOUR STREQUAL "ubuntu16.10")
+		set(CPACK_GENERATOR "DEB")
+		set(CPACK_DEBIAN_PACKAGE_DEPENDS  "libwebsockets7, libjson-c3, libssl1.0.0, libev4, libncurses5, libc6, ca-certificates")
+		set(CPACK_SYSTEM_NAME "yakkety")
+
+	elseif(TARGET_PACKAGE_FLAVOUR STREQUAL "ubuntu17.04")
+		set(CPACK_GENERATOR "DEB")
+		set(CPACK_DEBIAN_PACKAGE_DEPENDS  "libwebsockets8, libjson-c3, libssl1.0.0, libev4, libncurses5, libc6, ca-certificates")
+		set(CPACK_SYSTEM_NAME "zesty")
+
+    elseif(TARGET_PACKAGE_FLAVOUR STREQUAL "centos7")
+        set(CPACK_GENERATOR "RPM")
+        set(CPACK_RPM_PACKAGE_RELEASE "1")
+        set(CPACK_RPM_PACKAGE_REQUIRES "libwebsockets, json-c, openssl-libs, libev, ncurses-libs")
+        set(CPACK_SYSTEM_NAME "el7")
+
+	elseif(TARGET_PACKAGE_FLAVOUR STREQUAL "fedora24")
+		set(CPACK_GENERATOR "RPM")
+		set(CPACK_RPM_PACKAGE_RELEASE "1")
+		set(CPACK_RPM_PACKAGE_REQUIRES "libwebsockets, json-c, openssl-libs, libev, ncurses-libs")
+		set(CPACK_SYSTEM_NAME "fc24")
+
+	elseif(TARGET_PACKAGE_FLAVOUR STREQUAL "fedora25")
+		set(CPACK_GENERATOR "RPM")
+		set(CPACK_RPM_PACKAGE_RELEASE "1")
+		set(CPACK_RPM_PACKAGE_REQUIRES "libwebsockets, json-c, openssl-libs, libev, ncurses-libs")
+		set(CPACK_SYSTEM_NAME "fc25")
+
+	elseif(TARGET_PACKAGE_FLAVOUR STREQUAL "fedora26")
+		set(CPACK_GENERATOR "RPM")
+		set(CPACK_RPM_PACKAGE_REQUIRES "libwebsockets, json-c, openssl-libs, libev, ncurses-libs")
+		set(CPACK_SYSTEM_NAME "fc26")
+
+	else(TARGET_PACKAGE_FLAVOUR STREQUAL "debian9")
+		message(FATAL_ERROR "unknown flavour ${TARGET_PACKAGE_FLAVOUR}")
+	endif(TARGET_PACKAGE_FLAVOUR STREQUAL "debian9")
+else(TARGET_PACKAGE_FLAVOUR)
+	set(CPACK_GENERATOR "TGZ")
+endif(TARGET_PACKAGE_FLAVOUR)
+
+if(CPACK_GENERATOR STREQUAL "DEB")
+	file(READ "${CMAKE_SOURCE_DIR}/pkg/description.txt" PACKAGE_DESCRIPTION)
+	execute_process(
+		COMMAND sh -c "fmt -w 78 | sed -e 's/^$/./' -e 's/^/ /'"
+		INPUT_FILE "${CMAKE_SOURCE_DIR}/pkg/description.txt"
+		OUTPUT_VARIABLE PACKAGE_DESCRIPTION)
+	set(CPACK_PACKAGE_DESCRIPTION_SUMMARY
+"${CPACK_PACKAGE_DESCRIPTION_SUMMARY}
+${PACKAGE_DESCRIPTION}")
+elseif(CPACK_GENERATOR STREQUAL "RPM")
+	set(CPACK_RPM_POST_INSTALL_SCRIPT_FILE "${CMAKE_SOURCE_DIR}/pkg/postinst")
+endif(CPACK_GENERATOR STREQUAL "DEB")
+
+include(CPack)
